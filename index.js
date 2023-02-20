6fa8303cc80897d8581455b44980480b7e3b880f
purwar1997 <shubhampurwar511@gmail.com>
2023-02-20 21:10:31 +0530

    added more paths

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const file = fs.readFileSync('./swagger.yaml', { encoding: 'utf8' });
 const swaggerDocument = YAML.parse(file);
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
 
 let courses = [
@@ -59,6 +62,92 @@ app.get('/api/v1/course/:courseId', (req, res) => {
     }
 
     res.status(200).json(course);
+  } catch (err) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.post('/course/add', (req, res) => {
+  try {
+    let { id, course, price } = req.body;
+
+    if (!(id && course && price)) {
+      throw new CustomError('Please provide all the details', 400);
+    }
+
+    price = Number(price);
+
+    if (isNaN(price) || price <= 0) {
+      throw new CustomError('Price should be a positive number', 400);
+    }
+
+    courses.push({ id, course, price });
+
+    res.status(201).json(courses);
+  } catch (err) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.put('/course/update/:courseId', (req, res) => {
+  try {
+    const { courseId } = req.params;
+    let { id, course, price } = req.body;
+
+    if (!courseId) {
+      throw new CustomError('Course ID not present', 400);
+    }
+
+    if (!(id && course && price)) {
+      throw new CustomError('Please provide all the details', 400);
+    }
+
+    price = Number(price);
+
+    if (isNaN(price) || price <= 0) {
+      throw new CustomError('Price should be a positive number', 400);
+    }
+
+    const courseIndex = courses.findIndex(({ id }) => id === courseId);
+
+    if (courseIndex === -1) {
+      throw new CustomError('Course not found', 404);
+    }
+
+    courses.splice(courseIndex, 1, { id, course, price });
+
+    res.status(201).json(courses);
+  } catch (err) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.delete('/course/delete/:courseId', (req, res) => {
+  try {
+    const { courseId } = req.params;
+
+    if (!courseId) {
+      throw new CustomError('Course ID not present', 400);
+    }
+
+    const courseIndex = courses.findIndex(({ id }) => id === courseId);
+
+    if (courseIndex === -1) {
+      throw new CustomError('Course not found', 404);
+    }
+
+    courses.splice(courseIndex, 1);
+
+    res.status(200).json(courses);
   } catch (err) {
     res.status(err.code || 500).json({
       success: false,
